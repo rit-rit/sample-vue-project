904d07c6cfe3b91f2fd541c0fef622d23fc6a0f7
rit80rit <rit80rit@gmail.com>
2018-01-22 22:51:12 +0900

    Vue tutorial * Declarative Rendering

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,9 +2,7 @@ import Vue from 'vue';
 
 const v = new Vue({
   el: '#app',
-  template: `
-  <div><div>Hello{{name}}!</div>
-  Name:<input v-model="name" type="text">
-  </div>`,
-  data: { name: 'World' },
+  template: `<div id="app">{{message}}</div>
+ `,
+  data: { message: 'Hollo World!' },
 });
