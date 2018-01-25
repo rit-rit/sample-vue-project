ae39129c33d5739f994f8e03d4eee9326d2ae34d
rit80rit <rit80rit@gmail.com>
2018-01-25 23:42:54 +0900

    Vue tutorial * Declarative Rendering 2(Single File Components)

---
+++ src/ts/App1-1.vue
@@ -0,0 +1,12 @@
+<template>
+<div>{{message}}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return { message: "Hello World!!" };
+  }
+});
+</script>
