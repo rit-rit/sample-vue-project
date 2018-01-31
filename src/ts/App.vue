4f89b09b5ca677e9a4a7d5c1e6cf36a9ff976a38
rit80rit <rit80rit@gmail.com>
2018-01-31 23:44:31 +0900

    Vue tutorial  * Composing with Components

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,16 +1,28 @@
 <template>
 <div>
-  <p>{{message}}</p>
-  <input v-model="message">
+  <ol>
+    <todo-item
+    v-for="item in groceryList"
+    v-bind:todo="item"
+    v-bind:key="item.id"
+    >
+    </todo-item>
+  </ol>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import todoItem from "../components/todoItem.vue";
+Vue.component("todo-item", todoItem);
 export default Vue.extend({
   data() {
     return {
-      message: "Hello Vue.js!"
+      groceryList: [
+        { id: 0, text: "Vegetables" },
+        { id: 1, text: "Cheese" },
+        { id: 2, text: "Whatever else humans are supposed to eat" }
+      ]
     };
   }
 });
---
+++ src/ts/App1-6.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <p>{{message}}</p>
+  <input v-model="message">
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      message: "Hello Vue.js!"
+    };
+  }
+});
+</script>
