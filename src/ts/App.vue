e8c7ddf54181b146026967ea40db00f8021b743b
rit80rit <rit80rit@gmail.com>
2018-02-01 23:04:41 +0900

    Vue tutorial  * Data and Methods

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,29 +1,22 @@
 <template>
 <div>
-  <ol>
-    <todo-item
-    v-for="item in groceryList"
-    v-bind:todo="item"
-    v-bind:key="item.id"
-    >
-    </todo-item>
-  </ol>
+  <p>{{obj.foo}}</p>
+  <button @click="obj.foo='baz'">Change</button>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import todoItem from "../components/todoItem.vue";
-Vue.component("todo-item", todoItem);
+let obj = {
+  foo: "bar"
+};
 export default Vue.extend({
   data() {
     return {
-      groceryList: [
-        { id: 0, text: "Vegetables" },
-        { id: 1, text: "Cheese" },
-        { id: 2, text: "Whatever else humans are supposed to eat" }
-      ]
+      obj
     };
   }
 });
+
+Object.freeze(obj);
 </script>
---
+++ src/ts/App1-7.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <ol>
+    <todo-item
+    v-for="item in groceryList"
+    v-bind:todo="item"
+    v-bind:key="item.id"
+    >
+    </todo-item>
+  </ol>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import todoItem from "../components/todoItem.vue";
+Vue.component("todo-item", todoItem);
+export default Vue.extend({
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: "Vegetables" },
+        { id: 1, text: "Cheese" },
+        { id: 2, text: "Whatever else humans are supposed to eat" }
+      ]
+    };
+  }
+});
+</script>
