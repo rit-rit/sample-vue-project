a3377bc1da3e7935b50092ceccef46fadaac9825
rit80rit <rit80rit@gmail.com>
2018-02-02 23:47:15 +0900

    Vue tutorial  * Instance Lifecycle Hooks  * Lifecycle Diagram

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,22 +1,45 @@
 <template>
 <div>
-  <p>{{obj.foo}}</p>
-  <button @click="obj.foo='baz'">Change</button>
+  <p>{{a}}</p>
+  <button @click="a='updated'">Update</button>
+  <button @click="$destroy()">Destroy</button>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-let obj = {
-  foo: "bar"
-};
 export default Vue.extend({
   data() {
     return {
-      obj
+      a: 1
     };
+  },
+  beforeCreate() {
+    console.log("Before Create");
+  },
+  created() {
+    console.log("Created");
+    console.log("a is: " + this.a);
+    console.log("has el:" + this.$el);
+  },
+  beforeMount() {
+    console.log("Before Mount");
+  },
+  mounted() {
+    console.log("el:" + this.$el);
+    console.log("Mounted");
+  },
+  beforeUpdate() {
+    console.log("Before Update");
+  },
+  updated() {
+    console.log("Updated");
+  },
+  beforeDestroy() {
+    console.log("Before Destroy");
+  },
+  destroyed() {
+    console.log("Destroyed");
   }
 });
-
-Object.freeze(obj);
 </script>
---
+++ src/ts/App2-1.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <p>{{obj.foo}}</p>
+  <button @click="obj.foo='baz'">Change</button>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+let obj = {
+  foo: "bar"
+};
+export default Vue.extend({
+  data() {
+    return {
+      a: 1
+    };
+  },
+  created() {
+    console.log(this.a);
+  }
+});
+Object.freeze(obj);
+</script>
