56464a84aca33b93348e617a2cb1700d0e8c990a
rit80rit <rit80rit@gmail.com>
2018-02-05 20:18:56 +0900

    Vue tutorial  * Directives(Modifiers)

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -2,6 +2,10 @@
 <div>
 <a v-bind:href="url">URL</a>
 <button @click="url=url+'A'">Change</button>
+<br>
+<form v-on:submit.prevent="onSubmit">aaaaa
+  <button type="submit" @click="submit">submit</button>
+</form>
 </div>
 </template>
 
