0b0e5b4f502f0153d9f14ae448145f0daee29ff8
rit80rit <rit80rit@gmail.com>
2018-02-06 19:40:54 +0900

    Vue tutorial  * Computed Properties

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,11 +1,7 @@
 <template>
 <div>
-<a v-bind:href="url">URL</a>
-<button @click="url=url+'A'">Change</button>
-<br>
-<form v-on:submit.prevent="onSubmit">aaaaa
-  <button type="submit" @click="submit">submit</button>
-</form>
+<p>Original message: "{{message}}"</p>
+<p>Cumputed reversed message:"{{reversedMessage}}"</p>
 </div>
 </template>
 
@@ -14,8 +10,16 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      url: "AAAA"
+      message: "Hello"
     };
+  },
+  computed: {
+    reversedMessage(): String {
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
+    }
   }
 });
 </script>
---
+++ src/ts/App3-2.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<a v-bind:href="url">URL</a>
+<button @click="url=url+'A'">Change</button>
+<br>
+<form v-on:submit.prevent="onSubmit">aaaaa
+  <button type="submit" @click="submit">submit</button>
+</form>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      url: "AAAA"
+    };
+  }
+});
+</script>
