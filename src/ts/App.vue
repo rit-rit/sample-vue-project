dfacea133b9923c07bc4058810ddbc2c1e2fc4d2
rit80rit <rit80rit@gmail.com>
2018-02-16 22:05:13 +0900

    Vue tutorial * v-if-else

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,14 +1,9 @@
 <template>
 <div>
-  <h1 v-if="ok">Yes</h1>
-  <h1 v-else>No</h1>
-  <button @click="reverse">Reverse</button>
-  <br>
-  <template v-if="ok">
-    <h1>Title</h1>
-    <p>Paragraph1</p>
-    <p>Paragraph2</p>
-  </template>
+  <h1 v-if="value==0">0</h1>
+  <h1 v-else-if="value==1">1</h1>
+  <h1 v-else>2</h1>
+  <button @click="transit">Transit</button>
 </div>
 </template>
 
@@ -17,10 +12,10 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  ok: boolean = true;
+  value: number = 0;
 
-  reverse(): void {
-    this.ok = !this.ok;
+  transit(): void {
+    this.value != 2 ? (this.value = this.value + 1) : (this.value = 0);
   }
 }
 </script>
---
+++ src/ts/App6-1.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1 v-if="ok">Yes</h1>
+  <h1 v-else>No</h1>
+  <button @click="reverse">Reverse</button>
+  <br>
+  <template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph1</p>
+    <p>Paragraph2</p>
+  </template>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component
+export default class App extends Vue {
+  ok: boolean = true;
+
+  reverse(): void {
+    this.ok = !this.ok;
+  }
+}
+</script>
+
