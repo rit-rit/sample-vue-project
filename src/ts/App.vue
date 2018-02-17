dc6a8a875f526a21cc06a8870bf1d975de62e33a
rit80rit <rit80rit@gmail.com>
2018-02-17 22:13:56 +0900

    Vue tutorial * Controlling Resuable Elements with key

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,9 +1,26 @@
 <template>
 <div>
-  <h1 v-if="value==0">0</h1>
-  <h1 v-else-if="value==1">1</h1>
-  <h1 v-else>2</h1>
-  <button @click="transit">Transit</button>
+<template v-if="loginType==='username'">
+  <label>Username</label>
+  <input placeholder="Enter your name">
+</template>
+<template v-else>
+  <label>Email</label>
+  <input placeholder="Enter your email address">
+</template>
+<br>
+<button @click="toggle">Toggle login Type</button>
+<br>
+<template v-if="loginType==='username'">
+  <label>Username</label>
+  <input placeholder="Enter your name" key="usename-input">
+</template>
+<template v-else>
+  <label>Email</label>
+  <input placeholder="Enter your email address" key="email-input">
+</template>
+<br>
+<button @click="toggle">Toggle login Type</button>
 </div>
 </template>
 
@@ -12,11 +29,12 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  value: number = 0;
+  loginType: String = "username";
 
-  transit(): void {
-    this.value != 2 ? (this.value = this.value + 1) : (this.value = 0);
+  toggle(): void {
+    this.loginType === "username"
+      ? (this.loginType = "email")
+      : (this.loginType = "username");
   }
 }
 </script>
-
