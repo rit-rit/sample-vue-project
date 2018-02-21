bfa2f36cf0afe96a6434511dea362e15a6d83e93
rit80rit <rit80rit@gmail.com>
2018-02-21 23:19:48 +0900

    Vue tutorial * Array Change Detection(Mutation Methods)

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,8 +1,15 @@
 <template>
 <div>
-  <div v-for="(value,key,index) in object">
+  <div v-for="(value,key,index) in items">
     {{index}}-{{key}}-{{value}}
   </div>
+  <button @click="push">Push</button>
+  <button @click="pop">Pop</button>
+  <button @click="shift">Shift</button>
+  <button @click="unshift">Unshift</button>
+  <button @click="slice">Slice</button>
+  <button @click="sort">Sort</button>
+  <button @click="reverse">Reverse</button>
 </div>
 </template>
 
@@ -11,10 +18,39 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  object: Object = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30
-  };
+  items: Object[] = [{ message: "Foo" }, { message: "Bar" }];
+  count: number = 0;
+  push(): void {
+    this.items.push({ message: "Baz" + this.count });
+    this.count = this.count + 1;
+  }
+
+  pop(): void {
+    this.items.pop();
+  }
+
+  shift(): void {
+    this.items.shift();
+  }
+  unshift(): void {
+    this.items.unshift({ message: "Baz" + this.count });
+    this.count = this.count + 1;
+  }
+
+  slice(): void {
+    this.items = this.items.slice(2);
+  }
+
+  sort(): void {
+    this.items.sort(function(a: any, b: any): number {
+      a = a["message"];
+      b = b["message"];
+      return a == b ? 0 : a > b ? 1 : -1;
+    });
+  }
+
+  reverse(): void {
+    this.items.reverse();
+  }
 }
 </script>
---
+++ src/ts/App7-2.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div v-for="(value,key,index) in object">
+    {{index}}-{{key}}-{{value}}
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component
+export default class App extends Vue {
+  object: Object = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+  };
+}
+</script>
