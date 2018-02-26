6fb5db869bb25960ad125fa74ba0396f4bf2489b
rit80rit <rit80rit@gmail.com>
2018-02-26 21:41:02 +0900

    Vue tutorial * Listening to Events

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,17 +1,7 @@
 <template>
 <div>
-<span v-for="n in 10">{{n}}</span>
-<br>
-<ul>
-  <template v-for="item in items">
-    <li>{{item.msg}}</li>
-    <li class="divider">{{item.msg}}</li>
-  </template>
-</ul>
-<br>
-<li v-for="todo in todos" v-if="!todo.isComplete">
-  {{todo.task}}
-</li>
+<button v-on:click="counter+=1">Add 1</button>
+<p>The button above has been clicked {{ counter}} times</p>
 </div>
 </template>
 
@@ -20,26 +10,7 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  items: Object[] = [{ msg: "aaaa" }, { msg: "bbb" }];
-  todos: Object[] = [
-    {
-      task: "AAA",
-      isComplete: true
-    },
-    {
-      task: "BBBB",
-      isComplete: false
-    },
-    {
-      task: "CCC",
-      isComplete: false
-    }
-  ];
+  counter: number = 0;
 }
 </script>
 
-<style scoped>
-.divider {
-  color: #f00;
-}
-</style>
