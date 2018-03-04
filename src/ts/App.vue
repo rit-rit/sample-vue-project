f5b3fe984edde4525413edd1b2169c3b77b6dedb
rit-rit <rit80rit@gmail.com>
2018-03-04 21:18:14 +0900

    Vue tutorial * Key Modifiers

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,34 +1,68 @@
 <template>
 <div>
-  <div v-bind="aaa">{{Date()}}</div>
-  <div v-on:click="doThis">
-    <a v-on:click="doThis">Simple Do this</a>
-  </div>
-  <div v-on:click="doThis">
-    <a v-on:click.stop="doThis">Stop Do this</a>
-  </div>
+  <dl>
+    <dt>
+      Enter
+    </dt>
+    <dd>
+      <input v-on:keyup.enter="hoge('Enter',$event)">
+    </dd>
+    <dt>
+      Tab
+    </dt>
+    <dd>
+      <input v-on:keyup.tab="hoge('Tab',$event)">
+    </dd>
+    <dt>
+      Delete
+    </dt>
+    <dd>
+      <input v-on:keyup.delete="hoge('Delete',$event)">
+    </dd>
+    <dt>
+      Escape
+    </dt>
+    <dd>
+      <input v-on:keyup.esc="hoge('Escape',$event)">
+    </dd>
+    <dt>
+      Space
+    </dt>
+    <dd>
+      <input v-on:keyup.space="hoge('Space',$event)">
+    </dd>
+    <dt>
+      Up
+    </dt>
+    <dd>
+      <input v-on:keyup.up="hoge('Up',$event)">
+    </dd>
+    <dt>
+      Down
+    </dt>
+    <dd>
+      <input v-on:keyup.down="hoge('Down',$event)">
+    </dd>
+    <dt>
+      Left
+    </dt>
+    <dd>
+      <input v-on:keyup.left="hoge('Left',$event)">
+    </dd>
+    <dt>
+      Right
+    </dt>
+    <dd>
+      <input v-on:keyup.right="hoge('Right',$event)">
+    </dd>
+    <dt>
+      PageDown
+    </dt>
+    <dd>
+      <input v-on:keyup.page-down="hoge('PageDown',$event)">
+    </dd>
 
-  <form submit>
-    <button action="submit">Simple Submit</button>
-  </form>
-  <form v-on:submit.prevent>
-    <button action="submit">Prevent Submit</button>
-  </form>
-
-  <div v-on:click="doThis">
-    <a href="#" v-on:click="doThis">Normal Do this</a>
-  </div>
-  <div v-on:click="doThis">
-    <a href="#" v-on:click.stop.prevent="doThis">Stop Prevent Do this</a>
-  </div>
-
-  <div v-on:click.capture="doThis">
-    <a v-on:click="doThat">Capture doThat</a>
-  </div>
-
-  <div v-on:click.self="doThis">
-    <a v-on:click="doThat">Self doThat</a>
-  </div>
+  </dl>
 </div>
 </template>
 
@@ -37,11 +71,8 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  doThis(event: Event): void {
-    alert("Do this");
-  }
-  doThat(event: Event): void {
-    alert("Do that");
+  hoge(message: string, event: Event): void {
+    alert("Message:" + message + ",Event:" + event.type);
   }
 }
 </script>
