c531fa5f0bdaf1feee09876ec56801ef2fa7f01f
rit-rit <rit80rit@gmail.com>
2018-03-06 22:36:54 +0900

    Vue tutorial * Form Input Bindings(Basic Usage)

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,55 +1,57 @@
 <template>
 <div>
-  <dl>
-    <dt>
-      Alt+C
-    </dt>
-    <dd>
-      <input v-on:keyup.alt.67="hoge('Alt+C',$event)">
-    </dd>
-    <dt>
-      Ctrl+any
-    </dt>
-    <dd>
-      <input v-on:keyup.ctrl="hoge('Ctrl+E',$event)">
-    </dd>
-    <dt>
-      Shift+any
-    </dt>
-    <dd>
-      <input v-on:keyup.shift="hoge('Shift',$event)">
-    </dd>
-    <dt>
-      Meta+any
-    </dt>
-    <dd>
-      <input v-on:keyup.meta="hoge('Meta',$event)">
-    </dd>
-    <dt>
-      Exact Ctrl
-    </dt>
-    <dd>
-      <button v-on:click.ctrl.exact="hoge('Exact Ctrl',$event)"></button>
-    </dd>
-    <dt>
-      Left Click
-    </dt>
-    <dd>
-      <input v-on:click.left="hoge('Left Click',$event)">
-    </dd>
-    <dt>
-      Middle Click
-    </dt>
-    <dd>
-      <input v-on:click.middle="hoge('Middle Click',$event)">
-    </dd>
-    <dt>
-      Right Click
-    </dt>
-    <dd>
-      <input v-on:click.right="hoge('Right Click',$event)">
-    </dd>
-  </dl>
+  <h1>Text</h1>
+    <input v-model="message1" placeholder="Edit me">
+    <p>Message is {{message1}}</p>
+  
+  <h1>Multiline Text</h1>
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line;">{{message2}}</p>
+  <br>
+  <textarea v-model="message2" placeholder="Add multiple lines"></textarea>
+  <h1>Checkbox</h1>
+  <input type="checkbox" id="checkbox1" v-model="checked1">
+  <label for="checkbox1">{{checked1}}</label>
+  <h2>Multiple checkboxes</h2>
+  <div id="example-3">
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames">
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+    <label for="mike">Mike</label>
+    <br>
+    <span>Checked names:{{checkedNames}}</span>
+  </div>
+  <h1>Radio button</h1>
+  <input type="radio" id="one" value="One" v-model="picked">
+  <label for="one">One</label>
+  <input type="radio" id="two" value="Two" v-model="picked">
+  <label for="two">Two</label>
+  <br>
+  <span>Picked:{{picked}}</span>
+  <h1>Select</h1>
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>Selected:{{selected}}</span>
+  <h2>Multiple select</h2>
+  <select v-model="selected2" multiple>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>Selected:{{selected2}}</span>
+  <h2>Dynamic options</h2>
+  <select v-model="selected3">
+    <option v-for="option in options" v-bind:value="option.value" v-bind:key="option.text">
+      {{option.text}}
+    </option>
+  </select>
+  <span>Selected:{{selected3}}</span>
 </div>
 </template>
 
@@ -58,9 +60,19 @@ import Vue from "vue";
 import Component from "vue-class-component";
 @Component
 export default class App extends Vue {
-  hoge(message: string, event: Event): void {
-    alert("Message:" + message + ",Event:" + event.type);
-  }
+  message1: string = "";
+  message2: string = "";
+  checked1: boolean = true;
+  checkedNames: string[] = [];
+  picked: string[] = [];
+  selected: string = "";
+  selected2: string[] = [];
+  selected3: string = "A";
+  options: object[] = [
+    { text: "One", value: "A" },
+    { text: "Two", value: "B" },
+    { text: "Three", value: "C" }
+  ];
 }
 </script>
 
