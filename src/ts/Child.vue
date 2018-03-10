934eeb34f4c9f0737c63ffe3afc1c4375adbd09d
rit-rit <rit80rit@gmail.com>
2018-03-10 22:08:25 +0900

    Vue tutorial * Components

---
+++ src/ts/Child.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h1>Child</h1>
+  <span>val:{{val}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  props: {
+    val: String
+  }
+})
+export default class Child extends Vue {}
+</script>
+
