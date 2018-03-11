9791b67ca8722db8c3b977ad2af7d541f442d02c
rit-rit <rit80rit@gmail.com>
2018-03-11 22:10:31 +0900

    Vue turtorial * Components

---
+++ src/ts/App10-1.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>App</h1>
+  <span>{{message}}</span>
+  <input v-model="message">
+  <br>
+  <child :val="message"></child>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Child from "./App10-1-Child.vue";
+@Component({
+  components: {
+    child: Child
+  }
+})
+export default class App extends Vue {
+  message: string = "hello";
+}
+</script>
+
