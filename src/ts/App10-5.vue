39e8f7c35c2b1b992c34d78d2cf3a3a8dfa8929d
rit-rit <rit80rit@gmail.com>
2018-03-16 07:51:23 +0900

    * Vue tutorial

---
+++ src/ts/App10-5.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1>I'm the parent title</h1>
+  <simpleSlotSample>
+    <p>This is some original content</p>
+    <p>This is some more original content</p>
+  </simpleSlotSample>
+    <h1>I'm the parent title</h1>
+  <simpleSlotSample>
+  </simpleSlotSample>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import SimpleSlotSample from "./SimpleSlotSample.vue";
+@Component({
+  components: {
+    simpleSlotSample: SimpleSlotSample
+  }
+})
+export default class App extends Vue {
+  value: string = "";
+}
+</script>
+
