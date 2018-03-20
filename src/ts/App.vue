8b4a2a906382e4410f876988e05da680273ba24b
rit-rit <rit80rit@gmail.com>
2018-03-20 19:50:07 +0900

    Vue tutorial * Transitioning Single Elements/Components

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,33 +1,29 @@
 <template>
-<div>
-  <component v-bind:is="currentView"></component>
-  <button @click="switchView">Switch</button>
-</div>
+  <div>
+    <button @click="show = !show">Toggle</button>
+    <transition name="fade">
+      <p v-if="show">Hello</p>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import Component from "vue-class-component";
-import SampleView1 from "./SampleView1.vue";
-import SampleView2 from "./SampleView2.vue";
-@Component({
-  components: {
-    sampleView1: SampleView1,
-    sampleView2: SampleView2
-  }
-})
+import Vue from "vue";
+@Component
 export default class App extends Vue {
-  currentView: any = "sampleView1";
-  isView1: boolean = true;
-  switchView(): void {
-    if (this.isView1) {
-      this.isView1 = false;
-      this.currentView = "sampleView2";
-    } else {
-      this.isView1 = true;
-      this.currentView = "sampleView1";
-    }
-  }
+  show: boolean = true;
 }
 </script>
 
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
