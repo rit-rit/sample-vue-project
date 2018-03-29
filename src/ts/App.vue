9b8a0519f7a6b4a2f931d5a9e3ba6891a1246e45
rit-rit <rit80rit@gmail.com>
2018-03-29 23:08:57 +0900

    Vue tutorial * List Transition

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,49 +1,30 @@
 <template>
   <div>
-    <button @click="add">Add</button>
-    <button @click="remove">Remove</button>
-    <transition-group name="list" tag="p">
-      <span v-for="item in items" :key="item" class="list-item">
+    <button @click="shuffle">Shuffle</button>
+    <transition-group name="flip-list" tag="ul">
+      <li v-for="item in items" :key="item">
         {{item}}
-      </span>
+      </li>
     </transition-group>
   </div>
 </template>
 
 <script lang="ts">
+import lodash from "lodash";
 import Component from "vue-class-component";
 import Vue from "vue";
 @Component
 export default class App extends Vue {
   items: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  nextNum: number = 10;
-  add(): void {
-    this.items.splice(this.randomIndex(), 0, this.nextNum++);
-  }
 
-  randomIndex(): number {
-    return Math.floor(Math.random() * this.items.length);
-  }
-
-  remove(): void {
-    this.items.splice(this.randomIndex(), 1);
+  shuffle(): void {
+    this.items = lodash.shuffle(this.items);
   }
 }
 </script>
 
 <style>
-.list-item {
-  display: inline-block;
-  margin-right: 10px;
-}
-.list-enter-active,
-.list-leave-active {
-  transition: all 1s;
-}
-
-.list-enter,
-.list-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+.flip-list-move {
+  transition: transform 1s;
 }
 </style>
---
+++ src/ts/App11-8.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="add">Add</button>
+    <button @click="remove">Remove</button>
+    <transition-group name="list" tag="p">
+      <span v-for="item in items" :key="item" class="list-item">
+        {{item}}
+      </span>
+    </transition-group>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+@Component
+export default class App extends Vue {
+  items: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  nextNum: number = 10;
+  add(): void {
+    this.items.splice(this.randomIndex(), 0, this.nextNum++);
+  }
+
+  randomIndex(): number {
+    return Math.floor(Math.random() * this.items.length);
+  }
+
+  remove(): void {
+    this.items.splice(this.randomIndex(), 1);
+  }
+}
+</script>
+
+<style>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
