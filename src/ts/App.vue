6004ba2ab2e74015495b5a1ebb01f2691fedce3c
rit-rit <rit80rit@gmail.com>
2018-04-08 22:18:33 +0900

    Vue tutorial * Custom Directives

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,31 +1,20 @@
 <template>
 <div>
-  <input v-model.number="firstNumber" type="number" step="20">+
-  <input v-model.number="secondNumber" type="number" step="20">=
-  {{result}}
-  <p>
-    <animated-integer v-bind:value="firstNumber"></animated-integer>+
-    <animated-integer v-bind:value="secondNumber"></animated-integer>=
-    <animated-integer v-bind:value="result"></animated-integer>
-  </p>
+  <input v-focus>
 </div>
 </template>
 
 <script lang="ts">
 import Component from "vue-class-component";
-import { Watch } from "vue-property-decorator";
-import AnimatedInteger from "./App12-3-Child.vue";
 import Vue from "vue";
-import { TweenLite } from "gsap";
 @Component({
-  components: { animatedInteger: AnimatedInteger }
-})
-export default class App extends Vue {
-  firstNumber: number = 20;
-  secondNumber: number = 40;
-
-  get result(): number {
-    return this.firstNumber + this.secondNumber;
+  directives: {
+    focus: {
+      inserted: function(el: HTMLElement): void {
+        el.focus();
+      }
+    }
   }
-}
+})
+export default class App extends Vue {}
 </script>
