6ce58b3950e7dfada9302b67619cadf532269fa8
rit-rit <rit80rit@gmail.com>
2018-04-09 23:31:39 +0900

    Vue tutorial * Directive Hook Arguments

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -1,20 +1,44 @@
 <template>
-<div>
-  <input v-focus>
+<div id="hook-arguments-example" v-demo:foo.a.b="message">
 </div>
 </template>
 
 <script lang="ts">
 import Component from "vue-class-component";
 import Vue from "vue";
+import { VNode } from "vue/types/vnode";
 @Component({
   directives: {
-    focus: {
-      inserted: function(el: HTMLElement): void {
-        el.focus();
+    demo: {
+      bind: function(
+        el: HTMLElement,
+        binding: { [key: string]: any },
+        vnode: VNode
+      ): void {
+        var s = JSON.stringify;
+        el.innerHTML =
+          "name:" +
+          s(binding.name) +
+          "<br>" +
+          "value: " +
+          s(binding.value) +
+          "<br>" +
+          "expression: " +
+          s(binding.expression) +
+          "<br>" +
+          "argument: " +
+          s(binding.arg) +
+          "<br>" +
+          "modifiers: " +
+          s(binding.modifiers) +
+          "<br>" +
+          "vnode keys: " +
+          Object.keys(vnode).join(", ");
       }
     }
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  message: string = "hello";
+}
 </script>
---
+++ src/ts/App14-1.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <input v-focus>
+</div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+@Component({
+  directives: {
+    focus: {
+      inserted: function(el: HTMLElement): void {
+        el.focus();
+      }
+    }
+  }
+})
+export default class App extends Vue {}
+</script>
