4f69e2864046a4bc196dd558221ca8d7e4465fe2
rit-rit <rit80rit@gmail.com>
2018-05-05 11:32:46 +0900

    Modified build file * Able to change components to bundle

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,76 +2,81 @@ const path = require('path');
 const webpack = require('webpack');
 const StringReplacePlugin = require('string-replace-webpack-plugin');
 
-module.exports = {
-  entry: './src/ts/main.ts',
-  output: {
-    path: path.resolve(__dirname, '../dist'),
+module.exports = (env) => {
+  if (typeof env.targetComponent === 'boolean') {
+    env.targetComponent = 'App';
+  }
+  return {
+    entry: './src/ts/main.ts',
+    output: {
+      path: path.resolve(__dirname, '../dist'),
 
-    publicPath: '/dist/',
-    filename: 'build.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /main\.ts$/,
-        loader: StringReplacePlugin.replace({
-          replacements: [
-            {
-              pattern: /\${APP}/,
-              replacement: function(match, p1, offset, string) {
-                return env;
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode,
-            // we map the "scss" and "sass" values for the lang attribute to
-            // the right configs here. other preprocessors should work out of
-            // the box, no loader config like this necessary.
-            scss: 'vue-style-loader!css-loader!sass-loader',
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          },
-          // other vue-loader options go here
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        },
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]',
-        },
-      },
-    ],
-  },
-  plugins: [new StringReplacePlugin()],
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      vue$: 'vue/dist/vue.esm.js',
+      publicPath: '/dist/',
+      filename: 'build.js',
     },
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-  },
-  performance: {
-    hints: false,
-  },
-  devtool: '#eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /main\.ts$/,
+          loader: StringReplacePlugin.replace({
+            replacements: [
+              {
+                pattern: /\${APP}/,
+                replacement: function(match, p1, offset, string) {
+                  return env.targetComponent;
+                },
+              },
+            ],
+          }),
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              // Since sass-loader (weirdly) has SCSS as its default parse mode,
+              // we map the "scss" and "sass" values for the lang attribute to
+              // the right configs here. other preprocessors should work out of
+              // the box, no loader config like this necessary.
+              scss: 'vue-style-loader!css-loader!sass-loader',
+              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+            },
+            // other vue-loader options go here
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]',
+          },
+        },
+      ],
+    },
+    plugins: [new StringReplacePlugin()],
+    resolve: {
+      extensions: ['.ts', '.js', '.vue', '.json'],
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
+      },
+    },
+    devServer: {
+      historyApiFallback: true,
+      noInfo: true,
+    },
+    performance: {
+      hints: false,
+    },
+    devtool: '#eval-source-map',
+  };
 };
 
 if (process.env.NODE_ENV === 'production') {
