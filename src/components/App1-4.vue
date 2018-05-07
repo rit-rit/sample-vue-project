1904d1f7488bff5624b88f7c6b496038b5e9ffc0
rit-rit <rit80rit@gmail.com>
2018-05-07 23:22:33 +0900

    Modified to use vue-class-component * App1-4

---
+++ src/components/App1-4.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <ol>
+    <li v-for="todo in todos" :key="todo">
+      {{ todo.text}}
+    </li>
+  </ol>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component
+export default class App extends Vue {
+  todos: {}[] = [
+    { text: "Learn JavaScript" },
+    { text: "Learn Vue" },
+    { text: "Build somthing awesome" }
+  ];
+}
+</script>
