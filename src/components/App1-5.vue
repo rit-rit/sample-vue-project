961ee79998dcdea878bea4fb8796e6acd1bfebfa
rit-rit <rit80rit@gmail.com>
2018-05-08 10:46:42 +0900

    Modified to use vue-class-component * App1-5

---
+++ src/components/App1-5.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <p>{{message}}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Conponent from "vue-class-component";
+
+@Conponent
+export default class App extends Vue {
+  message: string = "Hello Vue.js!";
+
+  reverseMessage(): void {
+    this.message = this.message
+      .split("")
+      .reverse()
+      .join("");
+  }
+}
+</script>
