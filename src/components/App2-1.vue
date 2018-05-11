92b1eed5c00062c820c58ece9e8f89c1530cb359
rit-rit <rit80rit@gmail.com>
2018-05-11 23:08:29 +0900

    Modified to use vue-class-component * App2-1

--- src/components/App2-1.vue
+++ src/components/App2-1.vue
@@ -7,18 +7,18 @@
 
 <script lang="ts">
 import Vue from "vue";
-let obj = {
+import Component from "vue-class-component";
+var obj = {
   foo: "bar"
 };
-export default Vue.extend({
-  data() {
-    return {
-      a: 1
-    };
-  },
+@Component
+export default class App extends Vue {
+  a: number = 1;
+  obj: {} = obj;
+
   created() {
     console.log(this.a);
   }
-});
+}
 Object.freeze(obj);
 </script>
