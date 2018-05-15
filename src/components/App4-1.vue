04a1f90f2c85a9fa905155842367752129559b53
rit-rit <rit80rit@gmail.com>
2018-05-15 22:54:19 +0900

    Modified to use vue-class-component * App4-1

---
+++ src/components/App4-1.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<p>Original message: "{{message}}"</p>
+<p>Cumputed reversed message:"{{reversedMessage}}"</p>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+@Component
+export default class App extends Vue {
+  message: string = 'Hello';
+
+  get reversedMessage(): String {
+    return this.message
+      .split('')
+      .reverse()
+      .join('');
+  }
+}
+</script>
