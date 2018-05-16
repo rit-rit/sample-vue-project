5b32183c30db74332e1f9adeb210652ab08cf3df
rit-rit <rit80rit@gmail.com>
2018-05-16 22:13:41 +0900

    Modified to use vue-class-component * App4-2

---
+++ src/components/App4-2.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<p>Computed now: "{{now}}"</p>
+<p>Method now:"{{methodnow()}}"</p>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+@Component
+export default class App extends Vue {
+  message: string = 'Hello';
+
+  get now(): String {
+    return Date.now().toString();
+  }
+  methodnow(): String {
+    return Date.now().toString();
+  }
+}
+</script>
