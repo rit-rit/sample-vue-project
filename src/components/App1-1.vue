a0c5137d172eb104dfcce49c51f60c4d8cf96287
rit-rit <rit80rit@gmail.com>
2018-05-20 22:33:47 +0900

    Format file

--- src/components/App1-1.vue
+++ src/components/App1-1.vue
@@ -1,12 +1,12 @@
 <template>
-<div>{{message}}</div>
+  <div>{ {message}}</div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Components from "vue-class-component";
+import Vue from 'vue'
+import Components from 'vue-class-component'
 @Components
 export default class App extends Vue {
-  message: string = "Hello World!!";
+  message: string = 'Hello World!!'
 }
 </script>
