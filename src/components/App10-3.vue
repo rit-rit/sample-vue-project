460a3a0e09a3db2f918a2f3a64a291fe88db6e11
rit-rit <rit80rit@gmail.com>
2018-05-26 23:10:33 +0900

    Change directory * App10-3

--- src/components/App10-3.vue
+++ src/components/App10-3.vue
@@ -1,27 +1,27 @@
 <template>
-<div>
   <div>
-    <p v-for="message in messages" v-bind:key="message">
-      {{message}}</p>
+    <div>
+      <p v-for="message in messages" v-bind:key="message">
+        {{message}}</p>
       <messageEventExample v-on:message="handleMessage"></messageEventExample>
+    </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { Prop } from "vue-property-decorator";
-import MessageEventExample from "./MessageEventExample.vue";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+import MessageEventExample from './MessageEventExample.vue'
 @Component({
   components: {
     messageEventExample: MessageEventExample
   }
 })
 export default class App extends Vue {
-  messages: string[] = [];
+  messages: string[] = []
   handleMessage(payload: any): void {
-    this.messages.push(payload.message);
+    this.messages.push(payload.message)
   }
 }
 </script>
