ed888c93452ee991907a1acc7ee3e9a8917fc8cb
rit-rit <rit80rit@gmail.com>
2018-05-28 22:52:23 +0900

    Change directory * App10-5

--- src/components/App10-4.vue
+++ src/components/App10-4.vue
@@ -1,18 +1,18 @@
 <template>
-<currency-input v-model="price"></currency-input>
+  <currency-input v-model="price"></currency-input>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import CurrencyInput from "./CurrencyInput.vue";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import CurrencyInput from './CurrencyInput.vue'
 @Component({
   components: {
     currencyInput: CurrencyInput
   }
 })
 export default class App extends Vue {
-  value: string = "";
+  value: string = ''
 }
 </script>
 
