8853ba765be34ef8d061f122966e4954ffa20a33
rit-rit <rit80rit@gmail.com>
2018-06-15 23:52:23 +0900

    Change directory * App14-2

---
+++ src/components/App14-2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="hook-arguments-example" v-demo:foo.a.b="message">
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+import { VNode } from 'vue/types/vnode'
+@Component({
+  directives: {
+    demo: {
+      bind: function(
+        el: HTMLElement,
+        binding: { [key: string]: any },
+        vnode: VNode
+      ): void {
+        var s = JSON.stringify
+        el.innerHTML =
+          'name:' +
+          s(binding.name) +
+          '<br>' +
+          'value: ' +
+          s(binding.value) +
+          '<br>' +
+          'expression: ' +
+          s(binding.expression) +
+          '<br>' +
+          'argument: ' +
+          s(binding.arg) +
+          '<br>' +
+          'modifiers: ' +
+          s(binding.modifiers) +
+          '<br>' +
+          'vnode keys: ' +
+          Object.keys(vnode).join(', ')
+      }
+    }
+  }
+})
+export default class App extends Vue {
+  message: string = 'hello'
+}
+</script>
