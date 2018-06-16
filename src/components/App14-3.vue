a3f425d546768fa8ba448871477aa43f1f11f30c
rit-rit <rit80rit@gmail.com>
2018-06-16 21:53:15 +0900

    Change directory * App14-3

---
+++ src/components/App14-3.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="hook-arguments-example" v-demo="{color:'white',text:'hello'}">
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+import { VNode } from 'vue/types/vnode'
+@Component({
+  directives: {
+    demo: {
+      bind: function(
+        el: HTMLElement,
+        binding: { [key: string]: any },
+        vnode: VNode
+      ): void {
+        console.log(binding.value.color)
+        console.log(binding.value.text)
+      }
+    }
+  }
+})
+export default class App extends Vue {
+  message: string = 'hello'
+}
+</script>
